945639d7234a9137b17f12b6d5f8e420f5106e46
hemesonbarroso <hemesonbarroso@gmail.com>
2022-04-16 04:43:49 -0400

    CargaInicial2

---
+++ controledegastos/controledegastos/settings/heroku.py
@@ -0,0 +1,16 @@
+
+import environ
+
+from controledegastos.settings.base import *
+
+env = environ.Env()
+
+DEBUG = env.bool("DEBUG",False)
+
+SECRET_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS =env.list("ALLOWED_HOSTS")
+
+DATABASES = {
+    "defalt": env.db(),
+}
